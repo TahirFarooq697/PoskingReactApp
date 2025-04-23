e1c612d6b7427f61fdfccce74fa9683d6cd42cc4
TahirFarooq697 <farooqtahir553@gmail.com>
2025-04-23 04:02:33 -0700

    Update homepage and Vite config for GitHub Pages deployment

--- posking/src/Layout/MainLayout.jsx
+++ posking/src/Layout/MainLayout.jsx
@@ -23,7 +23,7 @@ const MainLayout = () => {
         ..........Bottom Main div.................. */}
         <div className="flex flex-1 mt-5 ">
             {/* bottom left sidebar div */}
-            <div className={` fixed top-16 left-0 h-full w-[80%] md:w-[20%] mt-5 px-3 transition-all duration-500 transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'}` }>
+            <div className={`fixed top-16 left-0 h-full w-[80%] md:w-[20%] mt-5 px-3 transition-all duration-500 transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'}` }>
                 <Sidebar />
               </div>
             {/* bottom right div */}
--- posking/vite.config.js
+++ posking/vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({
- 
-  plugins: [
-    tailwindcss(),
-  ],
-})
 // import { defineConfig } from 'vite'
 // import tailwindcss from '@tailwindcss/vite'
-
 // export default defineConfig({
-//   base: "PoskingReactApp", 
+ 
 //   plugins: [
 //     tailwindcss(),
 //   ],
 // })
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  base: "/PoskingReactApp/", 
+  plugins: [
+    tailwindcss(),
+  ],
+})
