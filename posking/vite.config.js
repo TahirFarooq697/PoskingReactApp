725d6dfccca385cefa2a7a1b9bdbbf3f7314e25d
TahirFarooq697 <farooqtahir553@gmail.com>
2025-04-23 00:41:57 -0700

    changes

--- posking/vite.config.js
+++ posking/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+ 
   plugins: [
     tailwindcss(),
   ],
 })
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   base: "PoskingReactApp", 
+//   plugins: [
+//     tailwindcss(),
+//   ],
+// })
